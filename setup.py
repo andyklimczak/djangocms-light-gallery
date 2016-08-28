0d64a9467af33635ab777a1faf6c033444ce4810
Andy Klimczak <andyklimczak@fastmail.com>
2016-08-28 15:47:04 -0400

    patch

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='djangocms-light-gallery',
-      version='0.4',
+      version='0.5',
       description='Light Gallery plugin for django CMS',
       url='https://github.com/andyklimczak/djangocms-light-gallery',
       author='Andy Klimczak',
