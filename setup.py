b65ebe5a8fb2ac99d3c8a9a9030cba34d1b95eff
Andy Klimczak <andyklimczak@fastmail.com>
2016-08-28 15:38:03 -0400

    manifest

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(name='djangocms-light-gallery',
-      version='0.3',
+      version='0.4',
       description='',
       url='https://github.com/andyklimczak/djangocms-light-gallery',
       author='Andy Klimczak',
       author_email='andyklimczak@fastmail.com',
       license='MIT',
       packages=find_packages(),
+      include_package_data=True,
       install_requires=[
           'django>=1.8',
           'django-cms>=3.0',
