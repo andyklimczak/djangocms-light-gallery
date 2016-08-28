47e61a06ca05a27d93d2262b77ba0b0bd731d1db
Andy Klimczak <andyklimczak@fastmail.com>
2016-08-28 15:43:57 -0400

    update and readme

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 DjangoCMS plugin for [Light Gallery](https://github.com/sachinchoolur/lightGallery)
+
+# Installation
+
+```
+pip install djangocms-light-gallery
+python manage.py migrate
+INSTALLED_APPS = [
+// ...
+'light_gallery',
+// ...
+```
--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup, find_packages
 
 setup(name='djangocms-light-gallery',
       version='0.4',
-      description='',
+      description='Light Gallery plugin for django CMS',
       url='https://github.com/andyklimczak/djangocms-light-gallery',
       author='Andy Klimczak',
       author_email='andyklimczak@fastmail.com',
       license='MIT',
       packages=find_packages(),
       include_package_data=True,
+      long_description=open('README.md').read(),
       install_requires=[
           'django>=1.8',
           'django-cms>=3.0',
