72f6af105417bbddb6517352e9796e7e0ec2eea0
Andy Klimczak <andyklimczak@fastmail.com>
2016-08-28 01:20:53 -0400

    testing2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='djangocms-light-gallery',
-      version='0.1',
+      version='0.2',
       description='',
       url='https://github.com/andyklimczak/djangocms-light-gallery',
       author='Andy Klimczak',
@@ -9,7 +9,7 @@ setup(name='djangocms-light-gallery',
       license='MIT',
       packages=find_packages(),
       install_requires=[
-          'django>=1.6',
+          'django>=1.8',
           'django-cms>=3.0',
           'django-filer',
           'django-sekizai',
